2fda4b7d59479fcd1e84367e8b16a52ebb749adf
Anas <63461034+nasruloh@users.noreply.github.com>
2022-09-19 15:17:51 +0700

    Update darkmode.js

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,4 +1,6 @@
-function myFunction() {
-   var element = document.body;
-   element.classList.toggle("dark-mode");
-}
+<script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+<script>
+    $(".theme-switch").on("click", () => {
+        $("body").toggleClass("light-theme");
+    });
+</script>
